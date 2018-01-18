18f802dd2ad3265647a4cb35e87f41d23823e054
野村ひとみ <nomurahitomi@hitominoMacBook.local>
2018-01-18 16:58:47 +0900

    Initial commit

---
+++ app/src/main/java/com/wacode/yuki/modernapp/ExampleFragmentPagerAdapter.java
@@ -0,0 +1,36 @@
+package com.wacode.yuki.modernapp;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by yuki on 2016/09/28.
+ */
+public class ExampleFragmentPagerAdapter extends FragmentPagerAdapter {
+    public ExampleFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return ExampleFragment.newInstance(android.R.color.holo_blue_light);
+            case 1:
+                return ExampleFragment.newInstance(android.R.color.holo_green_light);
+
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return "ページ" + (position + 1);
+    }
+}
